5ba7560ec950372c14daf86ddf340df7e04bf94b
Sevastian <sevastian.vascan7@gmail.com>
2025-06-04 09:19:27 +0300

    chore: Add interface StudentDAO and class StudentDAOImoplementation.

---
+++ src/main/java/pixel/academy/CRUD_app/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package pixel.academy.CRUD_app.dao;
+
+import pixel.academy.CRUD_app.entity.Student;
+
+public interface StudentDAO {
+    void save (Student theStudent);
+}
---
+++ src/main/java/pixel/academy/CRUD_app/dao/StudentDAOImplementation.java
@@ -0,0 +1,13 @@
+package pixel.academy.CRUD_app.dao;
+
+import org.springframework.stereotype.Repository;
+import pixel.academy.CRUD_app.entity.Student;
+
+@Repository
+public class StudentDAOImplementation implements StudentDAO{
+
+    @Override
+    public void save(Student theStudent) {
+
+    }
+}
