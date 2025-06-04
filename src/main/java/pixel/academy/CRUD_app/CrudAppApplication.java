02c4782512e3e341a893d2810fdef8273bfb85f8
Sevastian <sevastian.vascan7@gmail.com>
2025-06-04 09:32:38 +0300

    Continue program.

--- src/main/java/pixel/academy/CRUD_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/CRUD_app/CrudAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pixel.academy.CRUD_app.dao.StudentDAO;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -13,10 +14,17 @@ public class CrudAppApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// Cream un obiect Studnet
+		// Salvam obiectul Student in baza de date folosind DAO
+		// Afisam ID-ul studentului salvat
 	}
 
 }
